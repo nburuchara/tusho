3e0070d9c26fa308215c10d8cf7846d105014f74
Norman Buruchara <normanburuchara90@gmail.com>
2025-02-23 16:44:20 +0300

    Settings section (LandingPg)

--- src/landing-pg/LandingPg.js
+++ src/landing-pg/LandingPg.js
@@ -2633,9 +2633,9 @@ const Styles = styled.div `
 
 .jipange-settings-selected-date-screen-complete-body-inner-body-mpesa-payment {
     width: 100%;
-    height: 100%;
+    height: 99.25%;
     position: relative;
-    // border: 1px solid black;
+    border: 1px solid black;
 }
 
 
