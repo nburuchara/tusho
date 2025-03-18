8aaabef21081b55410c88f0259ead14656733de1
Norman Buruchara <normanburuchara90@gmail.com>
2025-03-18 10:15:22 +0300

    Home main section (LandingPg)

--- src/search-terms/SearchTerms.js
+++ src/search-terms/SearchTerms.js
@@ -1,12 +1,18 @@
 const results = [
 
+    {"id": "998573", "name": "Change my profile picture", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+
+    {"id": "998573", "name": "Add new delivery address", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+
+    {"id": "998573", "name": "Update delivery address", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+
     {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
     {"id": "998573", "name": "How do I know when my Jipange order will arrive?", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
     {"id": "998573", "name": "How can I add a product to 'Shop Pamoja'?", "category": "FAQ", "subCat1": "Shop Pamoja", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "Can I pay with cash on delivery?", "category": "FAQ", "subCat1": "Payment", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "Can I pay for my delivery  with cash?", "category": "FAQ", "subCat1": "Payment", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
    
   
