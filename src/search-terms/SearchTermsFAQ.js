6bdc4598fba1b3b1ec1d405dd7ce4702104badaf
Norman Buruchara <normanburuchara90@gmail.com>
2025-03-23 09:45:37 +0300

    FAQ settings (LandingPg)

--- src/search-terms/SearchTermsFAQ.js
+++ src/search-terms/SearchTermsFAQ.js
@@ -1,22 +1,22 @@
 const results = [
 
-    {"id": "998573", "name": "Where can I find discounts?", "category": "Question", "subCat1": "General", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "You can find discounted items by browsing 'Promo' products which are listed at the top of the shopping section. You can also activate 'Shop Pamoja' to find products that are being sold in bulk at a discount."},
+    {"id": "998573", "name": "Where can I find discounts?", "category": "General", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "You can find discounted items by browsing 'Promo' products which are listed at the top of the shopping section. You can also activate 'Shop Pamoja' to find products that are being sold in bulk at a discount."},
 
-    {"id": "998573", "name": "Will the delivery cost always be Kshs. 99?", "category": "Question", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "In the near future, we anticipate that the delivery price will remain the same. We have relationships with strategic partners arond the city that allows us to maintain this price level."},
+    {"id": "998573", "name": "Will the delivery cost always be Kshs. 99?", "category": "Delivery", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "In the near future, we anticipate that the delivery price will remain the same. We have relationships with strategic partners arond the city that allows us to maintain this price level."},
 
-    {"id": "998573", "name": "Can I add to my order once it's already on the way?", "category": "Question", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "It depends. Since we don't source most of our items from a single supermarket Once you are matched with your driver, we will notify you when the driver is picking up your items. I"},
+    {"id": "998573", "name": "Can I add to my order once it's already on the way?", "category": "Delivery", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "It depends. Since we don't source most of our items from a single supermarket Once you are matched with your driver, we will notify you when the driver is picking up your items. I"},
 
-    {"id": "998573", "name": "Can I schedule the specific time of delivery for Jipange?", "category": "Question", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : ""},
+    {"id": "998573", "name": "Can I schedule the specific time of delivery for Jipange?", "category": "Delivery", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : ""},
 
-    {"id": "998573", "name": "Is there a minimum order value?", "category": "Question", "subCat1": "General", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : ""},
+    {"id": "998573", "name": "Is there a minimum order amount to use the service? If so, how much is it?", "category": "General", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : ""},
 
-    {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "Delivery", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "How do I know when my Jipange order will arrive?", "category": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How do I know when my Jipange order will arrive?", "category": "Delivery", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "How can I add a product to 'Shop Pamoja'?", "category": "Shop Pamoja","img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How can I add a product to 'Shop Pamoja'?", "category": "Shop Pamoja", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "Can I pay for my delivery  with cash?", "category": "Payment", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "Can I pay for my delivery  with cash?", "category": "Payment", "subCat1": "Question", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
    
   
