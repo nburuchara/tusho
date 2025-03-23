4859eb4e28fd15a6554d40ead2e20f0681b6ca5c
Norman Buruchara <normanburuchara90@gmail.com>
2025-03-23 08:56:48 +0300

    FAQ settings (LandingPg)

--- src/search-terms/SearchTermsFAQ.js
+++ src/search-terms/SearchTermsFAQ.js
@@ -1,6 +1,6 @@
 const results = [
 
-    {"id": "998573", "name": "Where can I find discounts?", "category": "FAQ", "subCat1": "General", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "You can find discounted items by browsing 'Promo' products which are listed at the top of the shopping section. You can also activate 'Shop Pamoja' to find products that are being sold in bulk at a discount."},
+    {"id": "998573", "name": "Where can I find discounts?", "category": "General", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "You can find discounted items by browsing 'Promo' products which are listed at the top of the shopping section. You can also activate 'Shop Pamoja' to find products that are being sold in bulk at a discount."},
 
     {"id": "998573", "name": "Add new delivery address", "category": "Delivery Information", "subCat1": "New address", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
@@ -8,13 +8,13 @@ const results = [
 
     {"id": "998573", "name": "Update delivery address", "category": "Delivery Information", "subCat1": "Edit address", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "How do I know when my Jipange order will arrive?", "category": "FAQ", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How do I know when my Jipange order will arrive?", "category": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "How can I add a product to 'Shop Pamoja'?", "category": "FAQ", "subCat1": "Shop Pamoja", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How can I add a product to 'Shop Pamoja'?", "category": "Shop Pamoja","img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "Can I pay for my delivery  with cash?", "category": "FAQ", "subCat1": "Payment", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "Can I pay for my delivery  with cash?", "category": "Payment", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
    
   
