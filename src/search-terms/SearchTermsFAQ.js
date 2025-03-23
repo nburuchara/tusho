9e2aa0564f7281474f3f2c270d8723be5a75d4ef
Norman Buruchara <normanburuchara90@gmail.com>
2025-03-23 13:34:45 +0300

    FAQ settings (LandingPg)

--- src/search-terms/SearchTermsFAQ.js
+++ src/search-terms/SearchTermsFAQ.js
@@ -6,11 +6,11 @@ const results = [
 
     {"id": "998573", "name": "Can I add to my order once it's already on the way?", "category": "Delivery", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "It depends. Since we source all of our goods from widely distributed wholesalers, and not from a single supermarket location, the item you want to add might not be at the location of your driver is picking up from. If your item is at the pick up location, your rider can pick it up for you."},
 
-    {"id": "998573", "name": "Can I schedule the specific time of delivery for Jipange?", "category": "Delivery", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : ""},
+    {"id": "998573", "name": "Can I schedule the specific time of delivery for Jipange?", "category": "Delivery", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : "At the moment this is not possible, the rider will contact you on the day of the order to confirm an ideal drop-off time. However, we are launching a time slot feature soon."},
 
-    {"id": "998573", "name": "Is there a minimum order amount to use the service? If so, how much is it?", "category": "General", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : ""},
+    {"id": "998573", "name": "Is there a minimum order amount to use the service? If so, how much is it?", "category": "General", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer" : "Yes, the minimum order value is 800."},
 
-    {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "Delivery", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "Delivery", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "Depending on your order, and our stock of items, delivery can take anywhere from 90 mins to 1 day to deliver. If you live on the outskirts of Nairobi - it will likely take anywhere from a few hours to a day to deliver."},
 
     {"id": "998573", "name": "How do I know when my Jipange order will arrive?", "category": "Delivery", "subCat1": "", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
