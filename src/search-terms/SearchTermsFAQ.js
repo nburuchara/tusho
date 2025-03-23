101f0bd56b793c31b98e128f87307bb0ce39fddd
Norman Buruchara <normanburuchara90@gmail.com>
2025-03-23 09:10:42 +0300

    FAQ settings (LandingPg)

--- src/search-terms/SearchTermsFAQ.js
+++ src/search-terms/SearchTermsFAQ.js
@@ -1,12 +1,12 @@
 const results = [
 
-    {"id": "998573", "name": "Where can I find discounts?", "category": "General", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "You can find discounted items by browsing 'Promo' products which are listed at the top of the shopping section. You can also activate 'Shop Pamoja' to find products that are being sold in bulk at a discount."},
+    {"id": "998573", "name": "Where can I find discounts?", "category": "Question", "subCat1": "General", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "You can find discounted items by browsing 'Promo' products which are listed at the top of the shopping section. You can also activate 'Shop Pamoja' to find products that are being sold in bulk at a discount."},
 
-    {"id": "998573", "name": "Will the delivery cost always be Kshs. 99?", "category": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "Will the delivery cost always be Kshs. 99?", "category": "Question", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top", "answer": "In the near future, we anticipate that the delivery price will remain the same. We have relationships with strategic partners arond the city that allows us to maintain this price level."},
 
-    {"id": "998573", "name": "Add new Jipange order", "category": "Jipange", "subCat1": "New order", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "Can I add to my order once it's already on the way?", "category": "Question", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
-    {"id": "998573", "name": "Update delivery address", "category": "Delivery Information", "subCat1": "Edit address", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
+    {"id": "998573", "name": "Can I schedule the time ", "category": "Question", "subCat1": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
     {"id": "998573", "name": "How long will my order take to arrive if I live on the outskirts of Nairobi?", "category": "Delivery", "img": "/assets/images/products/min-maid-product.webp", "lastCat": "top"},
 
