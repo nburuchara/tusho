428a50aec4bf5a52ed3ad4591c6f46e029d2fcfe
Norman Buruchara <normanburuchara90@gmail.com>
2025-05-02 17:54:06 +0300

    Home main section (jipange header)

--- src/landing-pg/LandingPg.js
+++ src/landing-pg/LandingPg.js
@@ -8820,8 +8820,8 @@ const Styles = styled.div `
     margin-top: 1.35rem;
     margin-left: -0.55rem;
     // left: (0, 50%);
-    width: 52.5px !important;
-    height: 49.5px !important;
+    width: 46.5px !important;
+    height: 42.5px !important;
     border: 0.1px solid transparent !important;
     border-top-left-radius: 8px;
     border-bottom-left-radius: 8px;
